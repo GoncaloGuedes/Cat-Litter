9cb1e6546b211c14840886ace252a613f2d455b2
goncaloguedes <goncalo.manuelguedes@gmail.com>
2023-11-23 20:11:11 +0000

    Add email settings

--- api/core/settings.py
+++ api/core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -164,3 +166,11 @@ DJOSER = {
         "user_create": ["rest_framework.permissions.IsAdminUser"],
     },
 }
+
+# Server email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
